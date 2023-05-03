89e0aa612126e58756820d24a881d79d9ce946ea
Andienekr <andienekaaa98@gmail.com>
2023-05-03 19:10:24 +0700

    tubes awal

---
+++ index.php
@@ -0,0 +1,445 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Tribun Unpas</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
+  </head>
+  <body>
+  <nav class="navbar navbar-expand-lg bg-dark" data-bs-theme="dark">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">Tribun Unpas
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon">
+        </span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">HOME
+            </a>
+            </li>
+            <li class="nav-item">
+            </li>
+            <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                NEWS
+            </a>
+            <ul class="dropdown-menu">
+                <li>
+                <a class="dropdown-item" href="#">VIRAL
+                </a>
+                </li>
+                <li>
+                <a class="dropdown-item" href="#">KOREAN
+                </a>
+                </li>
+                <li>
+                <a class="dropdown-item" href="#">FILM & MUSIC
+                </a>
+                </li>
+            </ul>
+            </li>
+            <li class="nav-item">
+            </li>
+        </ul>
+        <form class="container-fluid justify-content-start">
+            <a href="login.php" class="btn btn-outline-success me-2" type="button">Login
+</a>
+            <a href="Registrasi.php" class="btn btn-sm btn-outline-secondary" type="button">Register
+</a>
+            </form>
+        <form class="d-flex" role="search">
+            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-success" type="submit">Search
+            </button>
+        </form>
+        </div>
+    </div>
+</nav>
+
+
+    <!--Main layout-->
+    <div class="container-fluid p-4">
+    <!--Section: News of the day 1-->
+    <section class="border-bottom pb-4 mb-5">
+        <div class="row gx-5">
+        <div class="col-md-6 mb-4">
+            <div class="bg-image hover-overlay ripple shadow-2-strong rounded-5" data-mdb-ripple-color="light">
+            <img src="https://akcdn.detik.net.id/visual/2023/03/21/tiara-andini-dan-alshad-ahmad-11_169.jpeg?w=900&q=90" class="img-fluid" />
+            <a href="#!">
+                <div class="mask" style="background-color: rgba(251, 251, 251, 0.15);"></div>
+            </a>
+            </div>
+        </div>
+
+        <div class="col-md-6 mb-4">
+            <span class="badge bg-danger px-2 py-1 shadow-1-strong mb-3">HOT VIRAL!</span>
+            <h4><strong>Inikah Bukti Alshad Ahmad Masih Berpacaran Dengan Tiara Andini</strong></h4>
+            <a href="https://www.insertlive.com/korea/20230329131429-191-306614/fans-kenang-sulli-pada-hari-ulang-tahun-sang-idola" class="btn btn-primary">Selengkapnya</a>
+        </div>
+        </div>
+    </section>
+
+    <!--Section: News of the day 2-->
+    <section class="border-bottom pb-4 mb-5">
+        <div class="row gx-5">
+
+        <div class="col-md-6 mb-4">
+            <span class="badge bg-danger px-2 py-1 shadow-1-strong mb-3">BERITA HARI INI</span>
+            <h4><strong>8 Moment Kompak Maudy Ayunda & Ibu Mertua Masak Kimbap Rendang di Korea</strong></h4>
+            <button type="button" class="btn btn-primary">Selengkapnya</button>
+        </div>
+
+        <div class="col-md-6 mb-4">
+            <div class="bg-image hover-overlay ripple shadow-2-strong rounded-5" data-mdb-ripple-color="light">
+            <img src="https://akcdn.detik.net.id/visual/2023/03/31/maudy-ayunda-10_169.jpeg?w=900&q=90" class="img-fluid" />
+            <a href="#!">
+                <div class="mask" style="background-color: rgba(251, 251, 251, 0.15);"></div>
+            </a>
+            </div>
+        </div>
+
+        </div>
+    </section>
+
+    <!--Section: News of the day 3-->
+    <section class="border-bottom pb-4 mb-5">
+        <div class="row gx-5">
+        <div class="col-md-6 mb-4">
+            <div class="bg-image hover-overlay ripple shadow-2-strong rounded-5" data-mdb-ripple-color="light">
+            <img src="https://akcdn.detik.net.id/visual/2023/03/31/jisoo-blackpink_169.jpeg?w=900&q=90" class="img-fluid" />
+            <a href="#!">
+                <div class="mask" style="background-color: rgba(251, 251, 251, 0.15);"></div>
+            </a>
+            </div>
+        </div>
+
+        <div class="col-md-6 mb-4">
+            <span class="badge bg-danger px-2 py-1 shadow-1-strong mb-3">TERBARU</span>
+            <h4><strong>Jisoo BLACKPINK Akhirnya Resmi Debut Solo Lewat FLOWER</strong></h4>
+            <button type="button" class="btn btn-primary">Selengkapnya</button>
+        </div>
+        </div>
+    </section>
+
+    <!--Berita dropdown-->
+    <section>
+        <div class="row gx-lg-5">
+        <div class="col-lg-4 col-md-12 mb-4 mb-lg-0">
+            <div>
+            <!-- Featured image -->
+            <div class="bg-image hover-overlay shadow-1-strong ripple rounded-5 mb-4"
+                data-mdb-ripple-color="light">
+                <img src="https://akcdn.detik.net.id/visual/2020/04/08/501609cc-c1e4-4ba4-8f80-42fc26951a4c_169.jpeg?w=900&q=90" class="img-fluid" />
+                <a href="#!">
+                <div class="mask" style="background-color: rgba(251, 251, 251, 0.15);"></div>
+                </a>
+            </div>
+
+            <!-- news film and music-->
+            <div class="row mb-3">
+                <div class="col-6">
+                <a href="" class="text-info">
+                    <i class="fas fa-plane"></i>
+                    FILM & MUSIC
+                </a>
+                </div>
+
+                <div class="col-6 text-end">
+                <u> 2 jam yang lalu</u>
+                </div>
+            </div>
+
+            <!-- news description 1-->
+            <a href="" class="text-dark">
+                <h5>
+                    <a href="https://www.insertlive.com/korea/20230329131429-191-306614/fans-kenang-sulli-pada-hari-ulang-tahun-sang-idola" class="text-dark">Lirik Lagu My Everything-Glen Fredly</a>    
+                </h5>
+
+            </a>
+
+            <hr />
+
+            <!-- News 1 -->
+            <a href="https://www.insertlive.com/korea/20230329131429-191-306614/fans-kenang-sulli-pada-hari-ulang-tahun-sang-idola" class="text-dark">
+                <div class="row mb-4 border-bottom pb-2">
+                    <div class="col-3">
+                        <img src="https://akcdn.detik.net.id/visual/2023/03/30/marvel-entertainment_169.jpeg?w=900&q=90"
+                        class="img-fluid shadow-1-strong rounded" alt="Marvel" />
+                    </div>
+
+                    <div class="col-9">
+                        <p class="mb-2"><strong>Jajaran Petinggi Marvel Entertaiment dipecat Disney</strong></p>
+                        <p>
+                        <u> 2 minggu lalu</u>
+                        </p>
+                    </div>
+                </div>
+            </a>
+
+            <!-- News 2-->
+            <a href="" class="text-dark">
+                <div class="row mb-4 border-bottom pb-2">
+                <div class="col-3">
+                    <img src="https://akcdn.detik.net.id/visual/2020/03/20/209e2f90-319d-4f30-94c5-18dfd69a4efe_169.jpeg?w=900&q=90"
+                    class="img-fluid shadow-1-strong rounded" alt="Reza Rahardian dan Acha s." />
+                </div>
+
+                <div class="col-9">
+                    <p class="mb-2"><strong>Reza Rahardian & Acha Septriasa Bersatu Lagi Lewat 'Surga di Bawah Langit'</strong></p>
+                    <p>
+                    <u>1 hari yang lalu</u>
+                    </p>
+                </div>
+                </div>
+            </a>
+
+            <!-- News 3-->
+            <a href="" class="text-dark">
+                <div class="row mb-4 border-bottom pb-2">
+                <div class="col-3">
+                    <img src="https://akcdn.detik.net.id/visual/2021/09/03/kebersamaan-ahmad-dhani-dan-maia-estianty-setelah-cerai-2_169.jpeg?w=900&q=90"
+                    class="img-fluid shadow-1-strong rounded" alt="Ahmad Dhani" />
+                </div>
+
+                <div class="col-9">
+                    <p class="mb-2"><strong>Adakah Konser Ultah Ke-51 Ahmad Dhani Ingin Manggung Bareng Ahmad Dhani</strong></p>
+                    <p>
+                    <u> 5 hari lalu</u>
+                    </p>
+                </div>
+                </div>
+            </a>
+
+            <!-- News 4-->
+            <a href="" class="text-dark">
+                <div class="row mb-4 border-bottom pb-2">
+                <div class="col-3">
+                    <img src="https://akcdn.detik.net.id/visual/2020/02/26/f2ccfaf0-000f-4dde-be14-e1bce48adeda_169.jpeg?w=900&q=90"
+                    class="img-fluid shadow-1-strong rounded" alt="Pildun" />
+                </div>
+
+                <div class="col-9">
+                    <p class="mb-2"><strong>Imbas Piala Dunia U-20 Batal di Indonesia, Weird Jenius Batal Rilis 'Glorious'</strong></p>
+                    <p>
+                    <u> 1 detik yang lalu</u>
+                    </p>
+                </div>
+                </div>
+            </a>
+            </div>
+        </div>
+
+        <div class="col-lg-4 col-md-6 mb-4 mb-lg-0">
+            <div>
+            <!-- news viral -->
+            <div class="bg-image hover-overlay shadow-1-strong rounded-5 ripple mb-4"
+                data-mdb-ripple-color="light">
+                <img src="https://akcdn.detik.net.id/visual/2020/10/05/bruce-lee-1_169.jpeg?w=900&q=90" class="img-fluid"
+                alt="Bruce lee" />
+                <a href="#!">
+                <div class="mask" style="background-color: rgba(251, 251, 251, 0.15);"></div>
+                </a>
+            </div>
+
+            <!-- Article data -->
+            <div class="row mb-3">
+                <div class="col-6">
+                <a href="" class="text-danger">
+                    <i class="fas fa-chart-pie"></i>
+                    VIRAL
+                </a>
+                </div>
+
+                <div class="col-6 text-end">
+                <u> 5 hari lalu</u>
+                </div>
+            </div>
+
+            <!-- news description -->
+            <a href="" class="text-dark">
+                <h5>Heboh Nama Asli Bruce-Lee adalah Badruddin Rusli, Benarkah?</h5>
+            </a>
+
+            <hr />
+
+            <!-- News 1 -->
+            <a href="" class="text-dark">
+                <div class="row mb-4 border-bottom pb-2">
+                <div class="col-3">
+                    <img src="https://akcdn.detik.net.id/visual/2020/04/10/963ec0b3-95c6-49bc-8d0c-90e303861e02_169.jpeg?w=900&q=90"
+                    class="img-fluid shadow-1-strong rounded" alt="United Kingdom" />
+                </div>
+
+                <div class="col-9">
+                    <p class="mb-2"><strong>Jadi Cinta Pertama, Mengapa Raja Charles III Tak Nikahi Carmilla Sejak Awal</strong></p>
+                    <p>
+                    <u> 1 jam yang lalu</u>
+                    </p>
+                </div>
+                </div>
+            </a>
+
+            <!-- News 2 -->
+            <a href="" class="text-dark">
+                <div class="row mb-4 border-bottom pb-2">
+                <div class="col-3">
+                    <img src="https://akcdn.detik.net.id/visual/2016/04/08/cc05fbc8-4647-4bf9-95ee-439da4a96f5c_169.jpg?w=900&q=90"
+                    class="img-fluid shadow-1-strong rounded" alt="Secret" />
+                </div>
+
+                <div class="col-9">
+                    <p class="mb-2"><strong>Misteri Royal Vault, Makam 'Rahasia' Para Keluarga Kerajaan Inggris</strong></p>
+                    <p>
+                    <u> 10 menit yang lalu</u>
+                    </p>
+                </div>
+                </div>
+            </a>
+
+            <!-- News 3-->
+            <a href="" class="text-dark">
+                <div class="row mb-4 border-bottom pb-2">
+                <div class="col-3">
+                    <img src="https://akcdn.detik.net.id/visual/2023/03/27/mumi-kepala-domba-jantan_169.webp?w=900&q=90"
+                    class="img-fluid shadow-1-strong rounded" alt="mesir" />
+                </div>
+
+                <div class="col-9">
+                    <p class="mb-2"><strong>Viral Ditemukan 2 Ribu Mumi Kepala Domba Jantan di Mesir</strong></p>
+                    <p>
+                    <u> 5 detik yang lalu</u>
+                    </p>
+                </div>
+                </div>
+            </a>
+
+            <!-- News 4-->
+            <a href="" class="text-dark">
+                <div class="row mb-4 border-bottom pb-2">
+                <div class="col-3">
+                    <img src="https://akcdn.detik.net.id/visual/2019/12/06/22d7c7d4-776d-49b5-bd5e-e7a9e8859ad1_169.jpeg?w=900&q=90"
+                    class="img-fluid shadow-1-strong rounded" alt="berita indonesia" />
+                </div>
+
+                <div class="col-9">
+                    <p class="mb-2"><strong>Siswa SMA Swasta Tewas di Tabrak R25, Pelaku Masih di Bawah Umur</strong></p>
+                    <p>
+                    <u> 30 menit yang lalu</u>
+                    </p>
+                </div>
+                </div>
+            </a>
+            </div>
+        </div>
+
+        <div class="col-lg-4 col-md-6 mb-4 mb-lg-0">
+            <div>
+            <!-- news korea-->
+            <div class="bg-image hover-overlay shadow-1-strong rounded-5 ripple mb-4"
+                data-mdb-ripple-color="light">
+                <img src="https://akcdn.detik.net.id/visual/2023/02/16/lee-seung-gi_169.jpeg?w=900&q=90" class="img-fluid"
+                alt="oppa" />
+                <a href="#!">
+                <div class="mask" style="background-color: rgba(251, 251, 251, 0.15);"></div>
+                </a>
+            </div>
+
+            <!-- article data-->
+            <div class="row mb-3">
+                <div class="col-6">
+                <a href="" class="text-warning">
+                    <i class="fas fa-code"></i>
+                    KOREA
+                </a>
+                </div>
+
+                <div class="col-6 text-end">
+                <u> 7 menit yang lalu</u>
+                </div>
+            </div>
+
+            <!-- news description -->
+            <a href="" class="text-dark">
+                <h5>Lee Seung Gi Bakal Konser di Jakarta 24 Juni</h5>
+
+            </a>
+
+            <hr />
+
+            <!-- News 1 -->
+            <a href="" class="text-dark">
+                <div class="row mb-4 border-bottom pb-2">
+                <div class="col-3">
+                    <img src="https://akcdn.detik.net.id/visual/2020/04/23/164b4428-8da9-47eb-bb42-c3de2d63dd5e_169.jpeg?w=900&q=90"
+                    class="img-fluid shadow-1-strong rounded" alt="film kriminal" />
+                </div>
+
+                <div class="col-9">
+                    <p class="mb-2"><strong>Cha Eun Woo Tawaran Main Film Kriminal 'Massal',Kenapa?</strong></p>
+                    <p>
+                    <u>3 detik yang lalu</u>
+                    </p>
+                </div>
+                </div>
+            </a>
+
+            <!-- News 2 -->
+            <a href="" class="text-dark">
+                <div class="row mb-4 border-bottom pb-2">
+                <div class="col-3">
+                    <img src="https://akcdn.detik.net.id/visual/2023/03/30/mark-nct_169.jpeg?w=900&q=90"
+                    class="img-fluid shadow-1-strong rounded" alt="nct" />
+                </div>
+
+                <div class="col-9">
+                    <p class="mb-2"><strong>Kkamchagiyaa! Mark NCT Rilis Lagu Solo 'Golden Hour'</strong></p>
+                    <p>
+                    <u> 30 menit yang lalu</u>
+                    </p>
+                </div>
+                </div>
+            </a>
+
+            <!-- News 3-->
+            <a href="" class="text-dark">
+                <div class="row mb-4 border-bottom pb-2">
+                <div class="col-3">
+                    <img src="https://akcdn.detik.net.id/visual/2023/02/10/drama-korea-our-blooming-youth-3_169.jpeg?w=900&q=90"
+                    class="img-fluid shadow-1-strong rounded" alt="Perdana Botanical Garden Kuala Lumpur" />
+                </div>
+
+                <div class="col-9">
+                    <p class="mb-2"><strong>5 Drama Korea Rating Tertinggi Minggu Keempat Maret 2023</strong></p>
+                    <p>
+                    <u> 2 hari lalu</u>
+                    </p>
+                </div>
+                </div>
+            </a>
+
+            <!-- News 4-->
+            <a href="" class="text-dark">
+                <div class="row mb-4 border-bottom pb-2">
+                <div class="col-3">
+                    <img src="https://akcdn.detik.net.id/visual/2020/10/14/peringati-1-tahun-kematian-sulli-dirindukan-sahabat-dan-fans_169.jpeg?w=900&q=90"
+                    class="img-fluid shadow-1-strong rounded" alt="Perdana Botanical Garden" />
+                </div>
+
+                <div class="col-9">
+                    <p class="mb-2"><strong>Fans Kenang Sulli Pada Hari Ulang Tahun Sang Idola</strong></p>
+                    <p>
+                    <u> 4 hari lalu</u>
+                    </p>
+                </div>
+                </div>
+            </a>
+            </div>
+    
+    <!--Main layout-->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.bundle.min.js" integrity="sha384-qKXV1j0HvMUeCBQ+QVp7JcfGl760yU08IQ+GpUo5hlbpg51QRiuqHAJz8+BrxE/N" crossorigin="anonymous"></script>
+  </body>
+</html>
